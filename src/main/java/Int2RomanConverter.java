3c92a317095679a50a88b50977ebd512a279575f
juanMoleroD <juanldimeco@gmail.com>
2022-02-24 22:19:40 +0000

    Added further test cases to continue development.

--- src/main/java/Int2RomanConverter.java
+++ src/main/java/Int2RomanConverter.java
@@ -1,33 +1,59 @@
+import java.sql.SQLOutput;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Int2RomanConverter {
 
-    public String convert(int i) {
+    private final List<Integer> numeralValuesList = List.of(1,5,10,50,100,500,1000);
 
-        HashMap<Integer, String> numeralReferences = new HashMap<Integer, String>(Map.of(
-                1, "I",
-                5,"V",
-                10, "X",
-                50, "L",
-                100, "C",
-                500, "D",
-                1000, "M"
-        ));
+    private final HashMap<Integer, String> numeralReferences =
+            new HashMap<Integer, String>(Map.of(
+                    1, "I",
+                    5,"V",
+                    10, "X",
+                    50, "L",
+                    100, "C",
+                    500, "D",
+                    1000, "M"
+    ));
 
-        if (numeralReferences.containsKey(i)) return numeralReferences.get(i);
+    public String convert(int numberToConvert) {
 
         StringBuilder romanNumber = new StringBuilder();
-        if (numeralReferences.containsKey(i+1)) romanNumber.append("I" + numeralReferences.get(i+1));
 
-        if (numeralReferences.containsKey(i-1)) romanNumber.append(numeralReferences.get(i-1) + "I");
+        for (int i = numeralValuesList.size() - 1, operatorNumber = numberToConvert; i >= 0; i--) {
+            if (isNumberInNumerals(operatorNumber, numeralValuesList.get(i))) {
+                romanNumber.append(numeralReferences.get(numeralValuesList.get(i)));
+                operatorNumber -= numeralValuesList.get(i);
 
+                if (operatorNumber > 0) { continue; }  else break;
+            }
 
+            if (isNumberMinusOneInNumerals(operatorNumber, numeralValuesList.get(i))) {
+                romanNumber.append("I").append(numeralReferences.get(numeralValuesList.get(i)));
+                operatorNumber -= numeralValuesList.get(i);
+                continue;
+            }
 
-
+            if (operatorNumber/numeralValuesList.get(i) >= 1) {
+                int roundedInt = operatorNumber/numeralValuesList.get(i);
+                for (int repetitions = 0 ; repetitions < roundedInt ; repetitions++ ) {
+                    romanNumber.append(numeralReferences.get(numeralValuesList.get(i)));
+                }
+                operatorNumber-= numeralValuesList.get(i);
+            }
+        }
 
         return romanNumber.toString();
+    }
+
+    private boolean isNumberMinusOneInNumerals(int number, int numberToCompareTo) {
+        return (number + 1) == numberToCompareTo;
+    }
+
+    private boolean isNumberInNumerals(int number, int numberToCompareTo) {
+        return number == numberToCompareTo;
     }
 
 }
--- src/test/java/Int2RomanConverterTest.java
+++ src/test/java/Int2RomanConverterTest.java
@@ -14,17 +14,51 @@ class Int2RomanConverterTest {
 
     @Test
     void converts_numeralsMinus1() {
-        assertEquals("IV", converter.convert(4));
+        //assertEquals("IV", converter.convert(4));
         assertEquals("IX", converter.convert(9));
         assertEquals("IL", converter.convert(49));
         assertEquals("IC", converter.convert(99));
         assertEquals("ID", converter.convert(499));
+
     }
 
     @Test
     void converts_numeralsPlus1() {
         assertEquals("VI", converter.convert(6));
         assertEquals("XI", converter.convert(11));
-
+        assertEquals("CI", converter.convert(101));
     }
+
+    @Test
+    void converts_numeralsPlus2() {
+        assertEquals("XII", converter.convert(12));
+        assertEquals("LII", converter.convert(52));
+        assertEquals("CII", converter.convert(102));
+        assertEquals("DII", converter.convert(502));
+    }
+
+    @Test
+    void converts_numeralsPlus5() {
+        assertEquals("XV", converter.convert(15));
+        assertEquals("LV", converter.convert(55));
+        assertEquals("CV", converter.convert(105));
+        assertEquals("DV", converter.convert(505));
+    }
+
+    @Test
+    void converts_complexNumerals() {
+        assertEquals("DCCXXVII", converter.convert(727));
+        assertEquals("LXXXVIII", converter.convert(88));
+        assertEquals("CXXXIV", converter.convert(134));
+        assertEquals("CCCXXXIII", converter.convert(333));
+        assertEquals("CCLVI", converter.convert(256));
+    }
+
+    @Test
+    void converts_higherLevelFoursAndNines() {
+        assertEquals("CMXCI",converter.convert(991));
+    }
+
+
+
 }
