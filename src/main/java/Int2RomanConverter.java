50cd8c9fa543531d8700e62cddc5e1f6906e09b3
juanMoleroD <juanldimeco@gmail.com>
2022-02-24 18:41:47 +0000

    Added tests and functionality for Numerals and Numerals+1 or -1

--- src/main/java/Int2RomanConverter.java
+++ src/main/java/Int2RomanConverter.java
@@ -1,8 +1,33 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Int2RomanConverter {
 
     public String convert(int i) {
-        StringBuilder romanNumber = new StringBuilder();
 
-        return "";
+        HashMap<Integer, String> numeralReferences = new HashMap<Integer, String>(Map.of(
+                1, "I",
+                5,"V",
+                10, "X",
+                50, "L",
+                100, "C",
+                500, "D",
+                1000, "M"
+        ));
+
+        if (numeralReferences.containsKey(i)) return numeralReferences.get(i);
+
+        StringBuilder romanNumber = new StringBuilder();
+        if (numeralReferences.containsKey(i+1)) romanNumber.append("I" + numeralReferences.get(i+1));
+
+        if (numeralReferences.containsKey(i-1)) romanNumber.append(numeralReferences.get(i-1) + "I");
+
+
+
+
+
+        return romanNumber.toString();
     }
+
 }
--- src/test/java/Int2RomanConverterTest.java
+++ src/test/java/Int2RomanConverterTest.java
@@ -4,12 +4,27 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class Int2RomanConverterTest {
 
+    Int2RomanConverter converter = new Int2RomanConverter();
+
     @Test
-    void hasConverterMethod() {
-        Int2RomanConverter converter = new Int2RomanConverter();
+    void converts_numbersOnNumerals() {
         assertEquals("I", converter.convert(1));
-        assertEquals("II", converter.convert(2));
+        assertEquals("V", converter.convert(5));
     }
 
+    @Test
+    void converts_numeralsMinus1() {
+        assertEquals("IV", converter.convert(4));
+        assertEquals("IX", converter.convert(9));
+        assertEquals("IL", converter.convert(49));
+        assertEquals("IC", converter.convert(99));
+        assertEquals("ID", converter.convert(499));
+    }
 
+    @Test
+    void converts_numeralsPlus1() {
+        assertEquals("VI", converter.convert(6));
+        assertEquals("XI", converter.convert(11));
+
+    }
 }
