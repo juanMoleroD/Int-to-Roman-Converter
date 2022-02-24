b8724bce0ebb1f540ca4c2a8be3f5aad84b3dfd2
juanMoleroD <juanldimeco@gmail.com>
2022-02-24 17:42:58 +0000

    updated Readme

---
+++ readme.md
@@ -0,0 +1,9 @@
+Funtion convert() takes int and converts it to roman numerals up to 1,000. 
+
+I = 1
+V = 5
+X = 10
+L = 50
+C = 100
+D = 500
+M = 1,000
--- src/main/java/Int2RomanConverter.java
+++ src/main/java/Int2RomanConverter.java
@@ -2,7 +2,7 @@ public class Int2RomanConverter {
 
     public String convert(int i) {
         StringBuilder romanNumber = new StringBuilder();
-        
+
         return "";
     }
 }
